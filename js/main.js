09649a1232fec552d943ae176d100ceb71db7fd0
Sushil <sushilkumar811544@gmail.com>
2024-01-28 09:27:45 +0530

    form validation

--- js/main.js
+++ js/main.js
@@ -21,3 +21,33 @@ document.querySelector(".signin").onclick = () => {
 document.querySelector(".close-form").onclick = () => {
   signInForm.classList.remove("active");
 };
+
+
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (email.value === '' || email.value == null) {
+    messages.push('Email is required')
+  }
+
+  if (password.value.length <= 6) {
+    messages.push('Password must be longer than 6 characters')
+  }
+
+  if (password.value.length >= 20) {
+    messages.push('Password must be less than 20 characters')
+  }
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
